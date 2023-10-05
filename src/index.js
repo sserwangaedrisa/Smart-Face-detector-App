98d838fa1d99ae1f5502e617fbb36f1b4366c203
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-10-05 18:12:42 +0300

    Changed the title to Face Detector

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import './index.css';
 import 'tachyons';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ReactHelmet from 'react-helmet';
+
+document.title = 'Face Detector';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <title>My Website</title>
     <App />
   </React.StrictMode>
 );

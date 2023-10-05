c4d226717d9b72e744a2ba72b197a773a4ac92ff
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-10-05 16:57:07 +0300

    Updates

---
+++ precache-manifest.2f3562a75b348753d4a6fc6425e935be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2564824705ec0cdb1421d7f0d985f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47636b7d3722fe66f5a3",
+    "url": "/static/css/2.85c36af2.chunk.css"
+  },
+  {
+    "revision": "1426a5a305788b971f59",
+    "url": "/static/css/main.1bf04b04.chunk.css"
+  },
+  {
+    "revision": "47636b7d3722fe66f5a3",
+    "url": "/static/js/2.8949b52d.chunk.js"
+  },
+  {
+    "revision": "b114cc85da504a772f040e3f40f8e46a",
+    "url": "/static/js/2.8949b52d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e614d96d7cd679a99f5b",
+    "url": "/static/js/3.d68ed608.chunk.js"
+  },
+  {
+    "revision": "1426a5a305788b971f59",
+    "url": "/static/js/main.42e93eea.chunk.js"
+  },
+  {
+    "revision": "3c4b02bf5bed499e457b",
+    "url": "/static/js/runtime-main.80e6ea09.js"
+  }
+]);

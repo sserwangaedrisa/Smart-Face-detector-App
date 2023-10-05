f410654ac820f4516e903d9851ec5f7d81dc5a23
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-10-05 17:56:19 +0300

    Changed the title of the website

--- src/index.js
+++ src/index.js
@@ -5,15 +5,14 @@ import './index.css';
 import 'tachyons';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactHelmet from 'react-helmet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <title>My Website</title>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
